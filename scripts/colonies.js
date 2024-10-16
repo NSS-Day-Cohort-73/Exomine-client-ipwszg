7845611bce22cd6ee1754470e8f252cb3d4eb2e9
mleelaw <mleelaw123@gmail.com>
2024-10-16 14:29:24 -0500

    adjusted develop branch with module commenting/database created

---
+++ scripts/colonies.js
@@ -0,0 +1,3 @@
+//export/define colonyResourcesHTML
+//define filterColonies()
+//should include fetch request for data from api
---
+++ scripts/governors.js
@@ -0,0 +1,3 @@
+//export/define govGetterHTML
+//should include event listener for gov drop down selection
+//should include fetch request for data from api
