d85dd53b5722e0d07c1dbda64ab253512fb2971d
mleelaw <mleelaw123@gmail.com>
2024-10-16 14:32:00 -0500

    Revert "Develop"

--- scripts/colonies.js
+++
@@ -1,3 +0,0 @@
-//export/define colonyResourcesHTML
-//define filterColonies()
-//should include fetch request for data from api
--- scripts/facilities.js
+++
@@ -1,6 +0,0 @@
-//export/define facilityGetterHTML
-//export/define facilityMineralsHTML (this creates radio buttons based on the facility selection + filterFacilities())
-//define filterFacilities()
-//should include event listener for facility drop down selection
-//should include event listener for mineral radio buttons
-//should include fetch request for data from api
