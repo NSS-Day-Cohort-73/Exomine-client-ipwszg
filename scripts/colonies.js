4b09860c8f40b8f4f1adf4b94091d9bddd521103
Kyrie Williams <wkyrie98@gmail.com>
2024-10-17 10:36:30 -0500

    Exported/defined coloniesResourcesHTML and defined filterColonies

--- scripts/colonies.js
+++ scripts/colonies.js
@@ -1,3 +1,30 @@
 //export/define colonyResourcesHTML
 //define filterColonies()
 //should include fetch request for data from api
+export const colonyResourcesHTML = async () => {
+    const response = await fetch("http://localhost:8088/governors?_expand=colony")
+    const colonies = await response.json()
+
+    let resourcesHTML = ""
+
+    for (const colony of colonies) {
+        if (changeEvent.target.value === colony.id) {
+            resourcesHTML += await filterColonies(colony.id)
+        }
+    } 
+    return resourcesHTML
+}
+
+const filterColonies = async (colonyId) => {
+    const response = await fetch("http://localhost:8088/coloniesminerals?_expand=mineral")
+    const filters = await response.json()
+
+    let coloniesHTML = ""
+
+    for (const filter of filters) {
+        if (colonyId === filter.colonyId) {
+            coloniesHTML += `${filter.mineralsAmount} tons of ${filter.mineral.name}`
+        }
+    }
+    return coloniesHTML
+}   
